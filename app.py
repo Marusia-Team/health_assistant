0523e0a173b0fe883e1bcf80eddc99ec9f2043e6
artem-nikolaev00 <starist2000@mail.ru>
2022-04-16 19:52:29 +0300

    first test

--- app.py
+++ app.py
@@ -6,27 +6,32 @@ from aiohttp import web
 import state
 import logic
 
-#Хост и порт, которые прослушивает наш вэб-сервер (Андрей, нужен айпишник и порт)
-HOST_IP = ""
-HOST_PORT =
+# Хост и порт, которые прослушивает наш вэб-сервер (Андрей, нужен айпишник и порт)
+
+HOST_IP = "127.0.0.1"
+HOST_PORT = 8080
+
 
 async def health_assistant(request_obj):
     # парсим входной жсон
     request = await request_obj.json()
+    print(request)
 
     # создаём ответ
     response = {}
     response["version"] = request["version"]
     response["session"] = request["session"]
     # закрывать сессию после текущего обращения к скиллу не надо, проставить руками, когда надо будет
-    response["response"] = { "end_session" : False }
+    response["response"] = {"end_session": False}
     user_state = state.get_state(request)
     session_state = user_state.get_session_state()
+    print(session_state)
 
     if request["session"]["new"]:
         new_state = logic.get_root_state()
     else:
         current_state = logic.get_state(session_state["current_state_id"])
+        print(request["request"]["command"])
         new_state = current_state.get_next_state(request["request"]["command"])
 
     response["response"]["text"] = new_state.get_text()
@@ -40,7 +45,6 @@ async def health_assistant(request_obj):
     return web.json_response(response)
 
 
-
 def init():
     # создание веб-приложения
     app = web.Application()
@@ -48,6 +52,7 @@ def init():
     app.router.add_post("/health_assistant", health_assistant)
     # запуск
     web.run_app(app, host=HOST_IP, port=HOST_PORT)
+
 
 if __name__ == "__main__":
     init()
